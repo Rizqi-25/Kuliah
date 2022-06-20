404305f15e5454c086cbec4fb8677e3c78d6a728
Muhammad Rizqi <88642423+Rizqi-25@users.noreply.github.com>
2022-06-20 16:26:09 +0700

    Update Mobil.php

--- Pemograman-Web/Semester 2/webrentalmobil/application/controllers/Mobil.php
+++ Pemograman-Web/Semester 2/webrentalmobil/application/controllers/Mobil.php
@@ -3,12 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mobil extends CI_Controller {
 
-    public function index()
+   public function index()
     {
         $this->load->model('mobil_model', 'mobil');
 
         $data['list_mobil'] = $this->mobil->getAll();
-        $this->load->view('dashboard/index', $data);
+        $data['merk'] = $this->mobil->getAllMerk();
+
+
+        $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
+        $this->load->view('mobil/index', $data);
+        $this->load->view('layout/footer');
     }
 
 }
