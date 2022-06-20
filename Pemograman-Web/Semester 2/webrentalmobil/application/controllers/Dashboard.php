209e59d0546f105e8c6bc764464d72c477931cf3
Muhammad Rizqi <88642423+Rizqi-25@users.noreply.github.com>
2022-06-20 16:26:38 +0700

    Update Dashboard.php

--- Pemograman-Web/Semester 2/webrentalmobil/application/controllers/Dashboard.php
+++ Pemograman-Web/Semester 2/webrentalmobil/application/controllers/Dashboard.php
@@ -3,18 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-    public function index(){
-        // $data = [
-        //     'nama'=>'Muhammad Rizqi',
-        //     'title'=>'STT NF 2022'
-        // ];
-        // // $this->load->view('dashboard/index', $data);
-        // $this->load->view('layout/header');
-        // $this->load->view('layout/sidebar');
-        $this->load->view('dashboard/index');
-        // $this->load->view('layout/footer');
+    public function index()
+    {
+        $this->load->model('mobil_model', 'mobil');
 
-
-
+        $data['list_mobil'] = $this->mobil->getAll();
+        $this->load->view('dashboard/index', $data);
     }
+}
 }
