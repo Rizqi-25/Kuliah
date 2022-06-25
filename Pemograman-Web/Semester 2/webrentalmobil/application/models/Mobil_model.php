e0ffc1b09a1ae7077f9658f854012dc0d95d1364
Muhammad Rizqi <88642423+Rizqi-25@users.noreply.github.com>
2022-06-25 12:16:08 +0700

    Update Mobil_model.php

--- Pemograman-Web/Semester 2/webrentalmobil/application/models/Mobil_model.php
+++ Pemograman-Web/Semester 2/webrentalmobil/application/models/Mobil_model.php
@@ -1,22 +1,17 @@
 <?php
 class Mobil_model extends CI_Model
 {
-  private $table = 'mobil';
-    private $table_merk = 'merk';
+    // private $table = 'mobil';
+    private $table = 'merkmobil';  // INI TABLE HASIL DARI CREATE VIEW MENGGABUNGKAN TABLE MERK DAN MOBIL
 
     public function getAll()
     {
-        //SELECT * FROM mobil
+        //SELECT * FROM view merkmobil -> INI TABLE HASIL DARI CREATE VIEW MENGGABUNGKAN TABLE MERK DAN MOBIL
+
         $query = $this->db->get($this->table);
         return $query->result();
     }
 
-    public function getAllMerk()
-    {
-        //SELECT * FROM merk
-        $query = $this->db->get($this->table_merk);
-        return $query->result();
-    }
 
     public function findById($id)
     {
